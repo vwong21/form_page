9ebda0a04579ca62c1084e723d63f853f93e3199
Vincent Wong <vincentwong5609@gmail.com>
2023-09-15 00:40:16 -0700

    added error messages

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,48 @@
-const form = document.getElementById('main-form');
-const submitBtn = document.getElementById('button');
+const form = document.getElementById('main-form'); //Main form variable
+const submitBtn = document.getElementById('button'); //Button variable
+
+//Variables for require inputs
+const firstName = document.getElementById('firstname')
+const lastName = document.getElementById('lastname')
+const email = document.getElementById('email')
+const stuNum = document.getElementById('studentnumber')
 
 form.addEventListener('submit', (e) => {
-    const firstName = document.getElementById('firstname').value.trim();
-    const lastName = document.getElementById('lastname').value.trim();
-    const email = document.getElementById('email').value.trim();
-    const stuNum = document.getElementById('studentnumber').value.trim();
+
+    //Variables for required inputs values
+    const firstNameValue = firstName.value.trim()
+    const lastNameValue = lastName.value.trim()
+    const emailValue = email.value.trim()
+    const stuNumValue = stuNum.value.trim()
     const studentNumberRegEx    = /^a0[0-9]{7}$/i;
-    
-    if (firstName == '') {
+
+    //Variables for error messages
+    const firstNameError = document.getElementById('first-name-error')
+    const lastNameError = document.getElementById('last-name-error')
+    const emailError = document.getElementById('email-error')
+    const stuNumError = document.getElementById('stu-num-error')
+    console.log(firstNameError.innerHTML)
+    if (firstNameValue == '') {
         e.preventDefault();
-        
+        firstNameError.innerHTML = "First name cannot be empty"
         console.log('first name is empty');
     };
 
-    if (lastName == '') {
+    if (lastNameValue == '') {
         e.preventDefault();
+        lastNameError.innerHTML = "Last name cannot be empty"
         console.log('last name is empty');
     };
 
-    if (email == '') {
+    if (emailValue == '') {
         e.preventDefault();
+        emailError.innerHTML = "Email cannot be empty"
         console.log('email is empty');
     };
 
-    if (!studentNumberRegEx.test(stuNum)){
+    if (!studentNumberRegEx.test(stuNumValue)){
         e.preventDefault();
+        stuNumError.innerHTML = "Student number doesn't match the criteria"
         console.log('student number is invalid');
     };
 });
